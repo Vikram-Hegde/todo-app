6ddad543ebc492de4482b0e2b93c10d0cad6b801
Vikram <vikramhegde1440@gmail.com>
2020-12-13 14:35:11 +0530

    crud is done

--- taskHandler.js
+++ taskHandler.js
@@ -1,5 +1,5 @@
 /* Global variables */
-// let i = 0;
+
 let tasks = [
   { task: "todo item 1", completed: true },
   { task: "todo item 2", completed: true },
@@ -11,12 +11,14 @@ const formInput = document.querySelector("form");
 const allSection = document.querySelector(".all-section");
 const pendingSection = document.querySelector(".pending-section");
 const completedSection = document.querySelector(".completed-section");
+const clearCompletedBtn = document.querySelector(".clear-completed");
 const itemLists = document.querySelectorAll(".item-list");
 let currentTab = document.querySelector(".active");
 let itemsCountWrapper = document.querySelector(".items-count");
+const card = document.querySelector(".card");
 
 const itemsCount = () =>
-  (itemsCountWrapper.textContent = currentTab.children.length + " items");
+  (itemsCountWrapper.textContent = currentTab.children.length + " tasks");
 
 const renderTasksInAll = () =>
   tasks.forEach(
@@ -24,19 +26,18 @@ const renderTasksInAll = () =>
       (allSection.innerHTML += `
       <div class="todo-item ${completed ? "done" : ""}">
       <div class="checkbox">
-        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
+        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9" class="icon-check">
           <path
             fill="none"
             stroke="#FFF"
             stroke-width="2"
             d="M1 4.304L3.696 7l6-6"
-            class="icon-check"
           />
         </svg>
         </div>
         <span>${task}</span>
         <div class="delete-icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" class="close-icon">
           <path
             fill="#494C6B"
             fill-rule="evenodd"
@@ -55,19 +56,18 @@ function renderTasks() {
     todoItem.className = "todo-item";
     todoItem.innerHTML = `
     <div class="checkbox">
-      <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
+      <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9" class="icon-check">
         <path
           fill="none"
           stroke="#FFF"
           stroke-width="2"
           d="M1 4.304L3.696 7l6-6"
-          class="icon-check"
         />
       </svg>
     </div>
     <span>${task}</span>
     <div class="delete-icon">
-      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" class="close-icon">
         <path
           fill="#494C6B"
           fill-rule="evenodd"
@@ -87,10 +87,10 @@ function renderTasks() {
   // add or remove border based on number of items left
   let count = itemsCount();
   if (count[0] === "0")
-    document.querySelector(".card__footer").style.borderTop = "none";
+    document.querySelector(".card__footer").style.boxShadow = "none";
   else
-    document.querySelector(".card__footer").style.borderTop =
-      "1px solid var(--v-l-grayish-blue)";
+    document.querySelector(".card__footer").style.boxShadow =
+      "0 1px var(--v-l-grayish-blue)";
 }
 renderTasks();
 function addNewTask(e) {
@@ -104,3 +104,62 @@ function addNewTask(e) {
   e.target.reset();
 }
 formInput.addEventListener("submit", addNewTask);
+
+function changeStatus(e) {
+  if (
+    !e.target.classList.contains("checkbox") &&
+    !e.target.classList.contains("icon-check")
+  )
+    return;
+  // const target =
+  //   e.target.nextElementSibling.innerText ||
+  //   e.target.parentElement.nextElementSibling.innerText;
+  // tasks.forEach((task) => {
+  //   if (task.task === target) {
+  //     task.completed = !task.completed;
+  //   }
+  // });
+  // renderTasks();
+}
+
+function changeStatus(e) {
+  if (e.target.classList.contains("checkbox")) {
+    const target = e.target.nextElementSibling.innerText;
+    tasks.forEach((task) => {
+      if (task.task === target) {
+        task.completed = !task.completed;
+      }
+    });
+    renderTasks();
+  }
+
+  if (e.target.classList.contains("icon-check")) {
+    const target = e.target.parentElement.nextElementSibling.innerText;
+    tasks.forEach((task) => {
+      if (task.task === target) {
+        task.completed = !task.completed;
+      }
+    });
+    renderTasks();
+  }
+}
+card.addEventListener("click", changeStatus);
+
+const removeCompleted = () => {
+  tasks = tasks.filter((task) => !task.completed);
+  renderTasks();
+};
+clearCompletedBtn.addEventListener("click", removeCompleted);
+
+function deleteTask(e) {
+  if (e.target.classList.contains("close-icon")) {
+    tasks = tasks.filter((task) => {
+      return !(
+        task.task === e.target.parentElement.previousElementSibling.textContent
+      );
+    });
+    renderTasks();
+  }
+}
+
+itemLists.forEach((list) => list.addEventListener("click", deleteTask));
--- uiHandler.js
+++ uiHandler.js
@@ -2,7 +2,6 @@ const themeChanger = document.querySelector(".theme-changer");
 const sunIcon = document.querySelector(".icon-sun");
 const moonIcon = document.querySelector(".icon-moon");
 const tabs = document.querySelector(".tabs");
-const card = document.querySelector(".card");
 itemsCount()
 
 themeChanger.addEventListener("click", (e) => {
@@ -25,19 +24,4 @@ function tabSwitcher(e) {
     itemsCount()
   }
 }
-
-function changeStatus(e) {
-  if (
-    !e.target.classList.contains("checkbox") &&
-    !e.target.classList.contains("icon-check")
-  )
-    return;
-  tasks.forEach((task) => {
-    if (task.task === e.target.nextElementSibling.textContent) {
-      task.completed = !task.completed;
-    }
-  });
-  renderTasks();
-}
-card.addEventListener("click", changeStatus);
 tabs.addEventListener("click", tabSwitcher);
